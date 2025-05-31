b09403156d0d5d01effe4bddb80359cfbf621844
Sakyey <gustaf.m.dahlqvist@gmail.com>
2025-05-31 02:21:13 +0200

    Changes for the header file for the playercharacter

--- Source/Workshop_1to5andGame/PlayerCharacter.h
+++ Source/Workshop_1to5andGame/PlayerCharacter.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
 #include "PlayerCharacter.generated.h"
 
 UCLASS()
@@ -18,6 +19,24 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	virtual void SetUpPlayerInputComponent(UInputComponent* InputComponent) override;
+
+	//Camera boom
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Camera")
+	class USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Catergory="Camera")
+	class UCameraComponent* FollowCamera;
+
+	//Enhanced input actions (set in the blueprint)
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	class UInputAction* IA_Move;
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	class UInputAction* IA_Look;
+
+	//HAndlers
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
 
 public:	
 	// Called every frame
